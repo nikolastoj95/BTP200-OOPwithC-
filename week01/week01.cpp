844b8203458992736ee1a3b2aa4978fe3d97f33a
Niki9526 <n01591561@humber.ca>
2026-01-06 17:33:10 -0500

    inital commit created cpp file

---
+++ week01/week01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// Agenda
+// Course introduction
+// Transitioning from C to C++
+// Type safety robustness of C++
+// Encapsulation, class, attributes, and behaviour
+// Namespace 
+// Modular programming 
+// Function overloading
+
+// everything starts here main()
+int main() {
+    std::cout << "Hello Nikola"<< std::endl;
+    return 0;
+}
