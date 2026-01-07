61434de017ed9a1733ee073bd69f762448eb2bad
Niki9526 <n01591561@humber.ca>
2026-01-06 20:02:09 -0500

    from lecture week 01 jan 06, in class

--- week01/week01.cpp
+++ week01/week01.cpp
@@ -12,15 +12,49 @@
 // everything starts here main()
 
 /*
-Object-Oriented Programming
-Object
-Encapsulation
-Abstraction
-Data Hiding
+Object-Oriented Programming - seeing program as a collection of objects 
+    interacting with each other 
+Object -  {data, method} what info it carrys, its behaviour 
+Encapsulation - (diff things put together ) -  process of putting {data, method} together
+Data Hiding -  encapsulation allows data hiding
+Abstraction -  Data abstraction, Behaviour abstraction (common of all students)
+
 Inheritance
 Polymorphism
 */
+// workshop 1 = relaized on Friday Jan 9, one week to work on it
+
+/* - Encapsulation
+
+struct Student { // structture , like Class
+    int id;
+    char name[32];
+    int birthyear;
+    char address[32];
+};
+
+
+
+
+*/
+
+/* procedural programming, functional programming
+* Diff between procedure and function
+func1 -obj
+func2- obj
+func3 - obj
+func4 -obj
+
+int main (){
+}
+
+Function -- returns a value
+Procedure -- does steps, 
+OOP- collection of objects, interacting with each other
+*/
 int main() {
-    std::cout << "Hello Nikola"<< std::endl;
+    char name[60] =  "Nikola";
+    int id = 2;
+    std::cout << "Name: " << name << " Id is " << id<< std::endl;
     return 0;
 }
