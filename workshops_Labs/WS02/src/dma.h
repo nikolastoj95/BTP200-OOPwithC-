59dff24d93aa799d2a469b5678cc4472b0e8d723
Niki9526 <n01591561@humber.ca>
2026-01-23 19:12:31 -0500

    added function sigurtures needed

--- workshops_Labs/WS02/src/dma.h
+++ workshops_Labs/WS02/src/dma.h
@@ -1,14 +1,17 @@
 #ifndef SENECA_DMA_H
 #define SENECA_DMA_H
-
+#include "cstr.h"
 namespace seneca {
     struct Samples {
         char* m_title; // Dynamically allocated title
         int* m_data; //Dynamically allocated array of ints
-        int m_size; // Number of elements in m_data
-        
+        int m_size; // Number of elements in m_data 
     };
 
+    Samples* CreateSamples (const char* title);
+    void add (Samples& S, const int data[], int size);
+    void append (int*& data, int size, const int appendedData[], int dataSize);
+    void freemem(Samples*& s);
 } // namespace seneca
 
 
