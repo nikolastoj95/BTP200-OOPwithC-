b1e6955e31745636f9389bc3a5bd0f32daeabf00
Niki9526 <n01591561@humber.ca>
2026-01-30 14:32:27 -0500

    added the private fields for the CC class and added the private methods and public methods like Constrc, set

--- workshops_Labs/WS03/src/CC.h
+++ workshops_Labs/WS03/src/CC.h
@@ -3,8 +3,43 @@
 #include <iostream>
 namespace seneca {
    class CC {
+      //private attributes
+      char* m_name{}; // dynamic array
+      unsigned long long m_ccNumber{};
+      short m_ccv{};
+      short m_expMonth{};
+      short m_expYear{};
+
+      void aloCopy (const char* name);
+      void deallocate();
+
+      /*two functions display and prnNumber came here provided*/
       void display(const char* name, unsigned long long number, short expYear, short expMon, short cvv) const;
       void prnNumber(unsigned long long no) const;
+      /**/
+      //validates the input
+      bool validate(const char* name, unsigned long long cardNo, short cvv, short expMon, short expYear)const;
+
+      public:
+
+      void set();
+      bool isEmpty() const;
+
+      void set(const char* cc_name, unsigned long long cc_no, short cvv, short expMon, short expYear);
+
+      void display()const;
+      //constructor/destructor
+
+      CC();
+
+      CC(const char* cc_name, unsigned long long cc_no, short cvv, short expMon=12, short expYear=26);
+
+      ~CC(); //deconstructor
+
+
+
+
+
    };
 
 }
