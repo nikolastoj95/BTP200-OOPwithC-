0a9d318f1051a2e16cae19127a9544838bd652ba
Niki9526 <n01591561@humber.ca>
2026-02-03 14:22:27 -0500

    intital commit, ,member function lecture

---
+++ week03/w3-class-memberFunc/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+/*
+Member Function (Method)
+Types of Member functions
+coding member functions
+    - declaration
+    - definition 
+    - calling
+*/
+
+/* 2nd video  Data Privacy 
+    - (private , public)
+Privacy in struct and class
+Accessing private data
+*/
+
+struct Country {
+    char mName[32];
+    unsigned int mArea;
+
+    // create function to access data or attributes to write or to read
+    // 1.  (write data in to it)  setter, or mutator , or modifer
+    /*inline*/ void set(const char* name, unsigned area); //treated as inline function  // to recieve name and area
+         
+
+    //2. (read data from it ) getter, query, accesser 
+        // reads data in the attributes and returns it
+    const char* getName() const { //query functions
+        return mName;
+    }
+    unsigned getArea() const{ // query functions
+        return mArea;
+    } 
+
+    //Getter function designed to access the data but do not make any changes to it
+        // query function
+    //Setter function designed to set data to the attributes, and make changes to them can not be const
+
+    //3.  Special member functions 
+    // Constructor and destructor - designed to execute logic during the 
+        // creation or destruction of object
+    // Purpose is to -- be created properly and distroyed properly 
+
+};
+
+// struct name :: function name (){}
+// below to access the class or struct set functions if it is defined in header file
+ void Country::set(const char* name, unsigned area){ //treated as inline function  // to recieve name and area
+         strcpy(mName, name);
+         mArea = area;
+    }
+
+int main (void){
+
+    Country ct;
+    // invokeing a member function (set, get)
+    ct.set("Serbia", 32546);
+
+    // strcpy(ct1.mName, "Serbia");
+
+    // ct1.mArea = 34543; // setting data
+
+    cout << "Country Name: " << ct.getName() << endl << "Country Area: " << ct.getArea() << endl;
+    return 0;
+};
