69fa7b45f6e0d20561557565d5383cfa677f116f
Niki9526 <n01591561@humber.ca>
2026-02-16 18:01:58 -0500

    default constructor code

--- week04-01_26-30/w4-0216/main.cpp
+++ week04-01_26-30/w4-0216/main.cpp
@@ -10,23 +10,44 @@ struct Marks{
 
 // default Constructor
 
+//customer Constructor
+
+
+
 class Student {
-        char* name; // to point to dynamic array
-        int id;
-        Marks marksObj;
+        char* name; // to point to dynamic array // default nullptr
+        int id; // 0 is default
+        Marks marksObj; // marks object - marks array default nullptr , size - 0
     public:
+        Student () { // Default Constructor
+            name = nullptr;
+            id =0;
+            marksObj.marks = nullptr;
+            marksObj.size = 0;
+        }; // construcor name same as class name
+
+        bool isDefault() {
+            if (name == nullptr || id == 0) {
+                return true;
+            } 
+            return 0;
+        }
+
+
         void set(int p_id, const char* p_name, const Marks& p_marks) {
             if (name != nullptr) {
                 delete[] name; // free previously allocated memory
             }
-            name = new char [strlen(p_name) + 1];
+            name = new char [strlen(p_name) + 1]; //allocated 
+
             strcpy(name, p_name);
             id = p_id;
             marksObj.size = p_marks.size;
             if (marksObj.marks != nullptr) {
                 delete[] marksObj.marks; // free previously allocated memory
             } 
-            marksObj.marks = new float [p_marks.size];
+            marksObj.marks = new float [p_marks.size]; // allocated
+
             for (int i =0; i< marksObj.size; i++) {
                 marksObj.marks[i] = p_marks.marks[i];
             }
@@ -60,15 +81,27 @@ int main (void) {
     mks.size =5; //setting size to 5
     mks.marks = new float[5] {44.44,55.55,66.66,77.77,88.88};
 
-    Student st;
+    Student st; // creates an object, invokes default constructor // sets data members to 0, nullptr
 
-    st.display();
+    if (!st.isDefault()) {
+        st.display();
+    } else {
+        cout << "object data is empty"<< endl;
+    }
+
+
 
     st.set(1001,"Jackson", mks);
 
-    st.display();
+    if (!st.isDefault()) {
+        st.display();
+    } else {
+        cout << "object is empty" << endl;
+    }
 
-    // st.deAllocate();
+    // st.display();
+
+    st.deAllocate();
 
 
     // std::cout << "Hello World!" << std::endl;
