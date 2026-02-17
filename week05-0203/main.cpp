f9c62c780f4bbf3e8cb34be18fed98d112ed7f5a
Niki9526 <n01591561@humber.ca>
2026-02-16 20:20:15 -0500

    increment, decrement operators done

--- week05-0203/main.cpp
+++ week05-0203/main.cpp
@@ -7,17 +7,20 @@ using namespace std;
 
 //video 1
 // incrementing(++), decrementing(--)
-// ++, --
+// ++, -- 
+// 
+
 class Marks {
 
     float m_marks [CAP]; //point to  dynamic array
-    int m_capacity;
-    int m_size;
+    int m_capacity; // size of the array 32 MAX -- fixed size array
+    int m_size; // less than or equal to capacity -- actual size of the array
     public:
         Marks(){ // default constructor
             m_size = 0;
-            m_capacity = CAP;
+            m_capacity = CAP; 
         }
+        // 2 arguments constructor
         Marks(int p_size, const float* p_marks) {
             m_capacity = CAP; // sets cap to 32
             if (p_size > 0 && p_marks != nullptr) {
@@ -32,14 +35,56 @@ class Marks {
             
         }
 
-       Marks& operator++(){
-            for (int i=0; i<m_size; i++) {
-                m_marks[i] +=1; // 99.1 -> 100.1
-                m_marks[i] = m_marks[i] > 100?100:m_marks[i];
+    //    Marks& operator++(){
+    //     // pre increment
+    //     // 99.1 -> 100.1
+    //         for (int i=0; i<m_size; i++) {
+    //             m_marks[i] +=1; // 99.1 -> 100.1
+    //             m_marks[i] = m_marks[i] > 100?100:m_marks[i];
+    //         }
+    //         return *this;
+    //     }
+        // pre incremate or prefix ++ operator ++mks
+        Marks& operator++(){
+            //code and logic here
+            // values in array by 1 increase
+            for (int i = 0; i< m_size; i++) {
+                m_marks[i] += 1; // 99.1 -> 100.1  do not want more than 100 limit to 100
+                m_marks[i] = m_marks[i]> 100 ? 100 : m_marks[i];
             }
             return *this;
         }
 
+        //post- increment operator  mks++
+        Marks operator++(int) {
+            Marks temp = *this; // copy current object to temp object
+            //call above ++ function in here
+            ++(*this);
+
+            return temp;
+
+        }
+
+         // pre decremate or prefix -- operator --mks
+        Marks& operator--(){
+            //code and logic here
+            // values in array by 1 increase
+            for (int i = 0; i< m_size; i++) {
+                m_marks[i] -= 1; // 99.1 -> 100.1  do not want more than 100 limit to 100
+                m_marks[i] = m_marks[i]< 0 ? 0 : m_marks[i];
+            }
+            return *this;
+        }
+
+        //post- dencrement operator  mks--
+        Marks operator--(int) {
+            Marks temp = *this; // copy current object to temp object
+            //call above ++ function in here
+            --(*this);
+
+            return temp;
+
+        }
 
         void display()  const {
                 if (m_size > 0){
@@ -55,25 +100,40 @@ class Marks {
             }
 };
 
-
-
-
-
-
-
-
-
-
 int main (void) {  
-    float marks [] ={55.55,66.66,77.77,99.8}; //object array
-    Marks mks (4,marks);
+    float marks [] ={55.55,66.66,77.77}; //object array
+    Marks mks (3,marks);
     mks.display();
-    //++mks;
+    //++mks; // pre increment // increment before  object
+    // mks++;
+
+    (++mks).display(); // operator function needs to return object return * this, Marks& 
+    //mks.display();
+    // 3 times
+    cout << "------" << endl;
+    mks++; // post incrumate
+    //mks.display();
+
+    (mks++).display();
+    mks.display();
+
+    //2 times 
+    // int x = 5;
+
+    // cout << ++x << endl;
+    // int y = 6+ ++x;
+    // ++ (++x);
+    // cout << y<<endl;
+    (--mks).display(); // preincremate
+    (mks--).display(); // post increment
+    mks.display();
+
+    
     
 
     //mks++;
-    (++mks).display();
-    int x=5;
-    cout << ++x <<endl;
+    // (++mks).display();
+    // int x=5;
+    // cout << ++x <<endl;
     return 0;
 }
