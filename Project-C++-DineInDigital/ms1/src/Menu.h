9a5ad44dc6a8aeafffe1aeeb4dd8fe62b810747b
Niki9526 <n01591561@humber.ca>
2026-03-09 23:34:42 -0400

    added citation on top

--- Project-C++-DineInDigital/ms1/src/Menu.h
+++ Project-C++-DineInDigital/ms1/src/Menu.h
@@ -1,3 +1,20 @@
+/* Citation and Sources...
+Final Project Milestone 1: 
+Module: Menu
+Filename: Menu.h
+Version 1.0
+Author: Nikola Stojanovic, StNo: 027 369 149, Email: nstojanovic@myseneca.ca
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2026/03/03  Preliminary release
+2024/03/06  started creating Menu module, header as per requirments
+2024/03/07 added MenuItem class added 4 attributes, constructor, display, valudation
+2024/03/07 checked and done
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code 
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
 #ifndef SENECA_MENU_H
 #define SENECA_MENU_H
 
