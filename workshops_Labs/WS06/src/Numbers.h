4579fb05bd1a5e9e0b5c7044709d3c7b5c1497ec
Niki9526 <n01591561@humber.ca>
2026-03-13 23:52:15 -0400

    added function sigtures for my added functions

--- workshops_Labs/WS06/src/Numbers.h
+++ workshops_Labs/WS06/src/Numbers.h
@@ -12,9 +12,33 @@
 //
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
+/* -----------------------------------------------------------
+Workshop 06
+Module: Numbers
+Filename: Numbers.h
+-----------------------------------------------------------
+Author: Nikola Stojanovic
+Student number: 027 369 149
+Email: nstojanovic@myseneca.ca
+Subject: BTP200NAA
+-----------------------------------------------------------
+Revision History
+------- --------- ------------------------------------------
+Version Date      Reason
+V0.7    2025/03/10 Opened File, read instructions, created the required methods, functions
+V0.8    2025/03/11 Added Functions required to implement numberCount, load, save, copy assignment, copy constructor,  append operatior, display 
+V0.9    2025/01/13 Ran tests, debugged, Works, added citation on the top
+V1.0    2025/01/13 Ready for submission
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code 
+that my professor provided to complete my work for function whatever.
+-----------------------------------------------------------
+*/
 #ifndef SENECA_NUMBERS_H_
 #define SENECA_NUMBERS_H_
 #include <iostream>
+
+using namespace std;
 namespace seneca {
    class Numbers {
       double* m_numbers;
@@ -25,13 +49,26 @@ namespace seneca {
       void setEmpty();
       void setFilename(const char* filename);
       void sort();
-   public:
-      Numbers();
-      Numbers(const char* filename);
       double average()const;
       double max()const;
       double min()const;
+
+      bool load();
+      void save() const;
+      int numberCount() const;
+   public:
+      Numbers();
+      Numbers(const char* filename);
+      ~Numbers();
+      Numbers(const Numbers& other);
+      Numbers& operator=(const Numbers& other);
+      Numbers& operator+=(double value);
+      ostream& display(ostream& ostr) const;
+      
+
    };
+   std::ostream& operator<<(std::ostream& os, const Numbers& N);
+   std::istream& operator>>(std::istream& istr, Numbers& N);
 }
 #endif // !SENECA_NUMBERS_H_
 
