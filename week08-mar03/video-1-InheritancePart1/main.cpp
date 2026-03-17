b1d6f661166f6f725ce9e293f2dee1e532064b50
Niki9526 <n01591561@humber.ca>
2026-03-17 14:45:10 -0400

    part 3 shadowing bc member finc

--- week08-mar03/video-1-InheritancePart1/main.cpp
+++ week08-mar03/video-1-InheritancePart1/main.cpp
@@ -8,6 +8,9 @@
 //video 2
 // keyword - protected 
 
+//video 3 - shawdowing 
+//
+using namespace std;
 class Animal { // base class
 // protected: // or do it like this
     char name[32];
@@ -36,24 +39,18 @@ class Animal { // base class
             return age;
         }
 
-
-
     public:
 
-    void set(const char* p_name, int p_age) {
-        if (p_age > 0 && p_name !=nullptr) {
-            age = p_age;
-            strcpy(name, p_name);
-
-        } else {
-            name[0] = '\0';
-            age=0;
-        }
+    void eat() {
+        std::cout << "This animal eats food." << std::endl;
     }
 
-    void print ()const{
+    std::ostream& print (std::ostream& os) const {
+       return os << "Name: " << name << ", age: "<< age <<std::endl;
+    }
+     void print ()const{
         //direct access
-        std::cout <<"Name: " << name  << ", age: " << age << std::endl;
+        std::cout <<"Name: " << name  << ", age: " << age << ", ";
     }
 };
 
@@ -68,6 +65,10 @@ class Dog: public Animal  {  // public  - mode of inheritance
                 strcpy(breed, p_breed);
             }
         }
+    const char* getBreed() const {
+        return breed;
+    }
+    
     public:
         
         void set(const char* p_name, int p_age, const char* p_breed) {
@@ -82,10 +83,21 @@ class Dog: public Animal  {  // public  - mode of inheritance
             }
 
         }  
+        void bark(){
+            std::cout<< "The dog barks." << std::endl;
+        }
+
+        void print() const {
+            std::cout << "Print- from - derived"<<std::endl;
+        }
         
-        void printB() {
-            std::cout <<"Name: " << getName()  << ", age: " << getAge() << ", ";
-            std::cout << "Breed: " << breed << std::endl;
+        void printD() const{
+            // std::cout <<"Name: " << getName()  << ", age: " << getAge() << ", ";
+            print();
+            print(std::cout) << std::endl;
+            // this print uses derived class print not base class print 
+
+            std::cout << "Breed: " << getBreed() << std::endl;
         }
     
 };
@@ -106,7 +118,7 @@ int main() {
     dog1.set("Max", 3, "Labrador");
     //dog1.setBreed("Labrador");
     // dog1.print();
-    dog1.printB();
+    dog1.printD();
 
     return 0;
 };
