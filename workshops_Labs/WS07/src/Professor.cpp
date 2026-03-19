47d46e278a0335516bc27b35cc747e6b26aca558
Niki9526 <n01591561@humber.ca>
2026-03-19 17:22:58 -0400

    added imports for Professor module .cpp file, and params Constructor

---
+++ workshops_Labs/WS07/src/Professor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Professor.h"
+#include "Utils.h"
+// #include "Employee.h"
+
+using namespace std;
+
+namespace seneca {
+
+
+
+     Professor::Professor(const char* name, const char* nameSubject, int numSections, size_t employeeNo, double salary ) : Employee(name, employeeNo, salary ) {
+
+
+     }
+
+    
+
+}
