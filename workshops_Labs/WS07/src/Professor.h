c39905f72b20cf4090d6a12f6ada35c4e6d9e577
Niki9526 <n01591561@humber.ca>
2026-03-20 14:44:05 -0400

    added cituation on top by me, removed testing comments

--- workshops_Labs/WS07/src/Professor.h
+++ workshops_Labs/WS07/src/Professor.h
@@ -1,3 +1,24 @@
+/* -----------------------------------------------------------
+Workshop 07
+Module: Professor
+Filename: Professor.h
+-----------------------------------------------------------
+Author: Nikola Stojanovic
+Student number: 027 369 149
+Email: nstojanovic@myseneca.ca
+Subject: BTP200NAA
+-----------------------------------------------------------
+Revision History
+------- --------- ------------------------------------------
+Version Date      Reason
+V0.8    2025/03/10 Opened File, read instructions, created the required methods, functions
+V0.9    2025/01/13 Ran tests, debugged, Works, added citation on the top
+V1.0    2025/01/13 Ready for submission
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code 
+that my professor provided to complete my work for function whatever.
+-----------------------------------------------------------
+*/
 #ifndef SENECA_PROFESSOR_H
 #define SENECA_PROFESSOR_H
 #include <iostream>
@@ -10,7 +31,7 @@ namespace seneca{
 
         // returns a double, does not modify Professor obj so const
         //calculates addtional pay based on Num of sections 
-        //  (1% * salary()) * m_numSections
+        // (1% * salary()) * m_numSections
        double devPay() const;
        
     public:
@@ -22,8 +43,6 @@ namespace seneca{
         Professor& operator=(const Professor&) = delete;
 
         ~Professor();
-        //delete[] m_subjectName;
-        // m_subjectName = nullptr;
 
         std::istream& read(std::istream& istr = std::cin );
         std::ostream& write(std::ostream& ostr = std::cout)const ;
