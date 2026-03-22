2b4f69e977ce89b526545da7a332ff1dc52a3504
Niki9526 <n01591561@humber.ca>
2026-03-21 22:24:10 -0400

    added cituation revision history

--- Project-C++-DineInDigital/ms2/src/Menu.cpp
+++ Project-C++-DineInDigital/ms2/src/Menu.cpp
@@ -7,10 +7,11 @@ Author: Nikola Stojanovic, StNo: 027 369 149, Email: nstojanovic@myseneca.ca
 Revision History
 -----------------------------------------------------------
 Date      Reason
-2026/03/03  Preliminary release
-2024/03/07  created Menu.cpp logic for member functions
-2024/03/08   added validations, statments to display
-2024/03/09  Debugged, Tested, run in main, works, done
+2026/02/25  Preliminary release
+2024/03/19  added constructors, to Menu class initalized Menu item attbutes as well added logic to constructor, deconsructor 
+2024/03/19  added  logic for operator overload << to add menu items titles, to object, added to select() which shows the whole menu, and able to select and display
+2024/03/20  tested, debugged, ran
+2024/03/21  checked and done
 -----------------------------------------------------------
 I have done all the coding by myself and only copied the code 
 that my professor provided to complete my workshops and assignments.
