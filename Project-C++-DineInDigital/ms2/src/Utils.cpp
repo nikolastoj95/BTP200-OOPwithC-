d7409c4bca2759cfffe471671065cd9a5d829566
Niki9526 <n01591561@humber.ca>
2026-03-21 21:55:14 -0400

    added two utilly functions to check int choosen, changed cituation to refelect nikola

--- Project-C++-DineInDigital/ms2/src/Utils.cpp
+++ Project-C++-DineInDigital/ms2/src/Utils.cpp
@@ -4,16 +4,19 @@
 // File	Utils.h
 // Version 0.1
 // started by Fardad
+// Added two Functions int getInt() and int getInt(min,max) -  Author: Nikola Stojanovic, StNo: 027 369 149, Email: nstojanovic@myseneca.ca
 // Description
 // utility function to be used in the project
 // Revision History
 // -----------------------------------------------------------
-// Name            Date            Reason
-//
+// Name                Date            Reason
+// Nikola Stojanovic   03/20/2026      Added two functions in ms2, int getInt() and int getInt(min,max) for input integer  entry  for the menu choice selection
+                                       Functions check if  integer entered if it is valid integer, no letters,spaces, and if the integer is less than the avabaile menu numbers and more than 0
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
 #include <iostream>
 #include "Utils.h"
+#include <sstream>
 using namespace std;
 namespace seneca {
    Utils ut;
@@ -53,6 +56,65 @@ namespace seneca {
          cstring++;
       }
       return cstring && *cstring == 0;
+   }
+
+   //Nikola
+   int Utils::getInt() {
+      int value{};
+      bool done = false;
+      while (!done) {
+         // Read the input
+         string line;
+         getline(cin, line);
+
+         //1. Empty input
+         if (line.empty()){
+            cout << "You must enter a value: ";
+            continue;
+         }
+
+         // 2.  Try  to parse int to see if it is valiate integer input
+         std::stringstream ss(line);
+         ss >> value;
+
+         if (ss.fail()) {
+            cout << "Invalid Integer: ";
+            continue;
+         }
+
+         //3. Check for trailing characters
+         char leftover{};
+         if (ss >>leftover) {
+            cout << "Only an integer please! ";
+            continue;
+         }
+
+         // here, passed valid integer
+
+         done = true;
+      }
+      //returns the valid integer
+      return value;
+   }
+
+   //Nikola
+
+   int Utils::getInt(int min, int max) {
+      int value{};
+      bool done = false;
+
+      while (!done) {
+          value = getInt(); // get the integer 
+          // checks if int is between 0 and max number of menu numbers 
+          // if more than max or less than 0 it fails
+          if (value < min || value > max) {
+            
+            cout << "Invalid value: [" << min << " <= value <= " << max << "], try again: ";
+          } else {
+            done = true;
+          }
+      }
+      return value;
    }
    
 }
