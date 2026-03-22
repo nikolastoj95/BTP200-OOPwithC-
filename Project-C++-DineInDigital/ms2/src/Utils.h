17bf57f72af85cc9491ee5bec861944786257089
Niki9526 <n01591561@humber.ca>
2026-03-21 21:55:53 -0400

    changed cituation to reflect me, added function sigtures for the two getInt to check the choosen ints

--- Project-C++-DineInDigital/ms2/src/Utils.h
+++ Project-C++-DineInDigital/ms2/src/Utils.h
@@ -4,12 +4,14 @@
 // File	Utils.h
 // Version 0.1
 // started by Fardad 
+// Added two Functions int getInt() and int getInt(min,max) -  Author: Nikola Stojanovic, StNo: 027 369 149, Email: nstojanovic@myseneca.ca
 // Description
 // utility function to be used in the project
 // Revision History
 // -----------------------------------------------------------
-// Name            Date            Reason
-//
+// Name                Date            Reason
+// Nikola Stojanovic   03/20/2026      Added two functions in ms2, int getInt() and int getInt(min,max) for input integer  entry  for the menu choice selection
+                                       
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
 #ifndef SENECA_UTILS_H
@@ -23,6 +25,11 @@ namespace seneca {
       int strlen(const char* str)const;
       bool isspace(char ch)const;
       bool isspace(const char* cstring)const;
+
+      // Nikola Stojanovic 027 369 149
+      int getInt();
+     // Nikola Stojanovic 027 369 149
+      int getInt(int min, int max);
    };
    extern Utils ut;   // makes the ut object available where Utils.h is included
                      // to call a Utils function call it as follows
