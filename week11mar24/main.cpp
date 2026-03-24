78dee604673fa7eeb81bfefb09c9cb308d3aeb9f
Niki9526 <n01591561@humber.ca>
2026-03-24 17:19:02 -0400

    inital commit - w11 mar24 topic

---
+++ week11mar24/main.cpp
@@ -0,0 +1,13 @@
+// Agenda - March25 week 11
+// - function template 
+// - class template
+// - constrained cast: static_cast, reinterpret_cast, const_cast, dynamic_cast
+
+// Function Template
+
+
+int main() {
+
+
+    return 0;
+}
