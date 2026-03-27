f21e9c19e47b68ded1ff629126f95f6f2a94729d
Niki9526 <n01591561@humber.ca>
2026-03-27 15:23:16 -0400

    added info in cituations

--- workshops_Labs/WS08/src/Shape.cpp
+++ workshops_Labs/WS08/src/Shape.cpp
@@ -11,9 +11,9 @@ Subject: BTP200NAA
 Revision History
 ------- --------- ------------------------------------------
 Version Date      Reason
-V0.8    2025/03/18 Opened File, read instructions, created the logic for the  required methods, constructors, validation, displaying, reading files 
-V0.9    2025/01/19 Created in write, read in Professor class to show the number of sections, subject name, and called base class method to display those first 
-V1.0    2025/01/20 Tested, Debuged , and Ready for submission
+V0.8    2025/03/26 Opened Files, read instructions, created Shape.cpp interface class (function defination)  for the two helpers for out/in operators 
+V0.9    2025/01/27 added logic for the out/in operator overloads, so it takes the Shape object and calls draw or getSpecs methods
+V1.0    2025/01/27 Tested, Debuged , and Ready for submission
 -----------------------------------------------------------
 I have done all the coding by myself and only copied the code 
 that my professor provided to complete my work for function whatever.
@@ -37,8 +37,6 @@ namespace seneca {
         return istr;
         
     }
-
-
 
 }
 
