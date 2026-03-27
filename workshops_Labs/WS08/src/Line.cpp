22dec19d0f02b95b9b32b10d139355729954b337
Niki9526 <n01591561@humber.ca>
2026-03-27 15:12:18 -0400

    added functions logic for Line class getSpecs, draw methods, constructors

---
+++ workshops_Labs/WS08/src/Line.cpp
@@ -0,0 +1,68 @@
+/* -----------------------------------------------------------
+Workshop 08
+Module: Line
+Filename: Line.cpp
+-----------------------------------------------------------
+Author: Nikola Stojanovic
+Student number: 027 369 149
+Email: nstojanovic@myseneca.ca
+Subject: BTP200NAA
+-----------------------------------------------------------
+Revision History
+------- --------- ------------------------------------------
+Version Date      Reason
+V0.8    2025/03/18 Opened File, read instructions, created the logic for the  required methods, constructors, validation, displaying, reading files 
+V0.9    2025/01/19 Created in write, read in Professor class to show the number of sections, subject name, and called base class method to display those first 
+V1.0    2025/01/20 Tested, Debuged , and Ready for submission
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code 
+that my professor provided to complete my work for function whatever.
+-----------------------------------------------------------
+*/
+#include <iostream>
+
+#include "Line.h"
+#include "Utils.h"
+
+namespace seneca {
+
+   Line:: Line() : LblShape() {
+        m_length = 0;
+   }
+
+    Line::Line(const char* label, int length) : LblShape(label) {
+        bool valid = length >= 0 ;
+
+        if (valid) {
+            m_length = length;
+        } else {
+            m_length = 0;
+        }
+
+        if (label && m_length < (int)ut.strlen(label)) {
+            m_length = ut.strlen(label);
+        }
+    }
+
+    void  Line::getSpecs(std::istream& istr) {
+        LblShape::getSpecs(istr); // calls getspecs function from base class to read the label
+
+        // read the length
+        istr >> m_length;
+
+        // Ignore reat of the line including new line
+        istr.ignore(1000, '\n');
+
+    }
+
+    void Line::draw (std::ostream& ostr )const {
+        if (m_length > 0 && label() != nullptr ) {
+            ostr << label() << '\n';
+            for (int i = 0; i < m_length ; i++) {
+                ostr << '=';
+            }
+            
+        }
+
+    }
+}
