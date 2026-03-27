190843c72fa73015282492224ffacf6d140f70b3
Niki9526 <n01591561@humber.ca>
2026-03-27 10:40:48 -0400

    defined fiunction sigtures for Shape.h virtual pure functions for drae, get specs and operator overloads

--- workshops_Labs/WS08/src/Shape.h
+++ workshops_Labs/WS08/src/Shape.h
@@ -1,3 +1,24 @@
+/* -----------------------------------------------------------
+Workshop 08
+Module: Shape
+Filename: Shape.h
+-----------------------------------------------------------
+Author: Nikola Stojanovic
+Student number: 027 369 149
+Email: nstojanovic@myseneca.ca
+Subject: BTP200NAA
+-----------------------------------------------------------
+Revision History
+------- --------- ------------------------------------------
+Version Date      Reason
+V0.8    2025/03/18 Opened File, read instructions, created the logic for the  required methods, constructors, validation, displaying, reading files 
+V0.9    2025/01/19 Created in write, read in Professor class to show the number of sections, subject name, and called base class method to display those first 
+V1.0    2025/01/20 Tested, Debuged , and Ready for submission
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code 
+that my professor provided to complete my work for function whatever.
+-----------------------------------------------------------
+*/
 #ifndef SENECA_SHAPE_H
 #define SENECA_SHAPE_H
 #include <iostream>
@@ -7,19 +28,25 @@ namespace seneca {
    class Shape {
    public:
       // Pure virtual function to draw the shape, implemented in derived classes
+      virtual void draw (std::ostream& ostr )const = 0;
       
-
       // Pure virtual function to get specifications of the shape, implemented in derived classes
+      virtual void getSpecs(std::istream& istr) = 0;
       
 
       // Virtual destructor to ensure proper cleanup of derived class objects
+
+      virtual ~Shape(){};
       
    };
 
    // Operator overload to handle output stream, relies on draw function
+
+   std::ostream& operator<< (std::ostream& ostr, const Shape& S);
    
 
    // Operator overload to handle input stream, relies on getSpecs function
+   std::istream& operator>> (std::istream& istr, Shape& S);
   
 
 }
