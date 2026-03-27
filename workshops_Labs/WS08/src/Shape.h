fdf67f9bd2bcec1a5c68bbe945166bf1ee5a15ec
Niki9526 <n01591561@humber.ca>
2026-03-27 15:23:28 -0400

    added version info in citations

--- workshops_Labs/WS08/src/Shape.h
+++ workshops_Labs/WS08/src/Shape.h
@@ -11,9 +11,9 @@ Subject: BTP200NAA
 Revision History
 ------- --------- ------------------------------------------
 Version Date      Reason
-V0.8    2025/03/18 Opened File, read instructions, created the logic for the  required methods, constructors, validation, displaying, reading files 
-V0.9    2025/01/19 Created in write, read in Professor class to show the number of sections, subject name, and called base class method to display those first 
-V1.0    2025/01/20 Tested, Debuged , and Ready for submission
+V0.8    2025/03/26 Opened Files, read instructions, created Shape.h interface class added the pure virtual functions draw(), getSpecs(), added virtual Decstructor
+V0.9    2025/01/27 added 2 helper functions, that overload >>/ << (output, input operators) to work with Shape objects using getSpecs and draw methods
+V1.0    2025/01/27 Tested, Debuged , and Ready for submission
 -----------------------------------------------------------
 I have done all the coding by myself and only copied the code 
 that my professor provided to complete my work for function whatever.
