44856ee2b6cd33d20616946a07b0e9b6a21b4b7c
Niki9526 <n01591561@humber.ca>
2026-03-27 10:41:13 -0400

    put function logic for operator overloads, ostr, istr

---
+++ workshops_Labs/WS08/src/Shape.cpp
@@ -0,0 +1,43 @@
+/* -----------------------------------------------------------
+Workshop 08
+Module: Shape
+Filename: Shape.cpp
+-----------------------------------------------------------
+Author: Nikola Stojanovic
+Student number: 027 369 149
+Email: nstojanovic@myseneca.ca
+Subject: BTP200NAA
+-----------------------------------------------------------
+Revision History
+------- --------- ------------------------------------------
+Version Date      Reason
+V0.8    2025/03/18 Opened File, read instructions, created the logic for the  required methods, constructors, validation, displaying, reading files 
+V0.9    2025/01/19 Created in write, read in Professor class to show the number of sections, subject name, and called base class method to display those first 
+V1.0    2025/01/20 Tested, Debuged , and Ready for submission
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code 
+that my professor provided to complete my work for function whatever.
+-----------------------------------------------------------
+*/
+
+#include <iostream>
+#include "Shape.h"
+
+using namespace std;
+
+namespace seneca {
+    
+    std::ostream& operator<< (std::ostream& ostr, const Shape& S) {
+        S.draw(ostr);
+        return ostr;
+    }
+
+    std::istream& operator>> (std::istream& istr, Shape& S) {
+        S.getSpecs(istr);
+        return istr;
+    }
+
+
+
+}
+
