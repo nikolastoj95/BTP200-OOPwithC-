63b9c261508671a30df22021bea571eabe759a39
Niki9526 <n01591561@humber.ca>
2026-04-04 00:57:45 -0400

    creaded header file for Ordering Module Ordering Class

---
+++ Project-C++-DineInDigital/ms4/src/Ordering.h
@@ -0,0 +1,24 @@
+#ifndef SENECA_ORDERING_H
+#define SENECA_ORDERING_H
+#include <iostream>
+#include "constants.h"
+
+using namespace std;
+
+namespace seneca {
+
+    class Ordering {
+        //attributes
+        unsigned int m_foodCounter;
+        unsigned int m_drinkCounter;
+        unsigned int m_billableCounter;
+        unsigned int m_billSerialNum;
+    
+    public:
+        
+
+    };
+
+
+}
+#endif
