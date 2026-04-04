639da51f37937181b9d69379e196bcd6a8c95c96
Niki9526 <n01591561@humber.ca>
2026-04-04 01:00:20 -0400

    added two constants tax, MaxNum Bill items

--- Project-C++-DineInDigital/ms4/src/constants.h
+++ Project-C++-DineInDigital/ms4/src/constants.h
@@ -4,6 +4,9 @@
 
 namespace seneca {
    const size_t MaximumNumberOfMenuItems = 20u;
+   const double Tax = 0.13;
+   const size_t MaximumNumberOfBillItems = 20u;
+
 }
 
 #endif // !SENECA_CONSTANTS_H
