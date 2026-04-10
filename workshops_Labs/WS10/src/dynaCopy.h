50fe8e43bcd6febe407f3ea7f62414a92d738b5d
Niki9526 <n01591561@humber.ca>
2026-04-10 14:59:25 -0400

    added citation  for dynaCopy.h

--- workshops_Labs/WS10/src/dynaCopy.h
+++ workshops_Labs/WS10/src/dynaCopy.h
@@ -1,3 +1,24 @@
+/* -----------------------------------------------------------
+Workshop 10
+Module: dynaCopy
+Filename: dynaCopy.h
+-----------------------------------------------------------
+Author: Nikola Stojanovic
+Student number: 027 369 149
+Email: nstojanovic@myseneca.ca
+Subject: BTP200NAA
+-----------------------------------------------------------
+Revision History
+------- --------- ------------------------------------------
+Version Date      Reason
+V0.8    2026/04/09  Opened files, read requirments, studyed the Name module, main.cpp, Utils.cpp
+V0.9    2026/04/09  Added the Function templates for functions to copy elements of one array to another and a function to print the elements of the array it is a generic type so any types can use the function
+V1.0    2026/04/09 Tested, Debuged ,and Ready for submission
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code 
+that my professor provided to complete my work for function whatever.
+-----------------------------------------------------------
+*/
 #ifndef SENECA_DYNACOPY_H
 #define SENECA_DYNACOPY_H
 #include <iostream>
